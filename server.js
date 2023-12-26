25111034b823488f5e15f827adfc1e56e05c03eb
Gowthami karnati <karnatigowthami43@gmail.com>
2023-12-26 13:48:14 +0530

    adding more

--- server.js
+++ server.js
@@ -82,25 +82,25 @@ app.get('/', (req, res) => {
 //     res.status(500).json({ error: 'Internal Server Error' });
 //   }
 // });
-app.get('/sendpostrequest', async (req, res) => {
-  try {
-    const url = 'https://riktam.tigersheet.com/api/sheet-api/get-records';
-    const headers = {
-      'Authorization': 'C9B53439FA03FB946C93E9AC9963070B221EC0E3CD66399A',
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    };  
-    const payload = {
-      'sheet_id': '78425244',
+// app.get('/sendpostrequest', async (req, res) => {
+//   try {
+//     const url = 'https://riktam.tigersheet.com/api/sheet-api/get-records';
+//     const headers = {
+//       'Authorization': 'C9B53439FA03FB946C93E9AC9963070B221EC0E3CD66399A',
+//       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+//     };  
+//     const payload = {
+//       'sheet_id': '78425244',
   
-    };
-    const response = await axios.post(url, payload, { headers });
-    console.log('Response from Tigersheet', response.data);
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error in post request:', error.message);
-    res.status(500).send('Internal Server Error');
-  }
-});
+//     };
+//     const response = await axios.post(url, payload, { headers });
+//     console.log('Response from Tigersheet', response.data);
+//     res.send(response.data);
+//   } catch (error) {
+//     console.error('Error in post request:', error.message);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
  
 
